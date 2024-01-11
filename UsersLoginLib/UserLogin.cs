3bbb3f04ddca5fcc5056ebf678746bf53bbcabd5
mayank5654 <mayankkaushal99@gmail.com>
2024-01-11 10:13:00 +0530

    intial commit

---
+++ UsersLoginLib/UserLogin.cs
@@ -0,0 +1,19 @@
+﻿namespace UsersLoginLib
+{
+    public class UserLogin
+    {
+        public string Login(string userName, string userPwd)
+        {
+            string result = "";
+            if (userName == "mayank" && userPwd == "mayank@123")
+            {
+                result = "Login success";
+            }
+            else
+            {
+                result = "Login Failed";
+            }
+            return result;
+        }
+    }
+}
